e5979529ac33d685ec4b5e0e0654ada4d9536e37
trizzo <trizzo@payfone.com>
2018-12-14 14:14:43 -0700

    Added 'create' functionality.

--- ppmtool/src/main/java/com/trizzo/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/trizzo/ppmtool/web/ProjectController.java
@@ -21,6 +21,7 @@ public class ProjectController {
 	@PostMapping("")
 	public ResponseEntity<Project> createNewProject(@RequestBody Project project)
 	{
+		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}
 }
