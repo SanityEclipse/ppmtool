6c97d2ded272de5831b2b82565f0cb045b461886
trizzo <trizzo@payfone.com>
2018-12-20 13:15:20 -0700

    added routing for views

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AddProject from "./components/project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
