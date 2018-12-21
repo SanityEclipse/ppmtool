217457731b1a408a2b49f6b4243ba41438b0b4fe
trizzo <trizzo@payfone.com>
2018-12-21 15:27:10 -0700

    Linked front end and back end.

--- ppmtool-react-client/src/components/project/AddProject.js
+++ ppmtool-react-client/src/components/project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor() {
@@ -26,13 +29,13 @@ class AddProject extends Component {
     event.preventDefault();
     const newProject = {
       projectName: this.state.projectName,
-      projectIdentifier: this.state.Identifier,
+      projectIdentifier: this.state.projectIdentifier,
       description: this.state.description,
       start_date: this.state.start_date,
       end_date: this.state.end_date
     };
 
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -107,4 +110,11 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createProject }
+)(AddProject);
