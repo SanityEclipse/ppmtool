08e89525b450d28a609fcdc9e9d581feb20dbb0b
trizzo <trizzo@payfone.com>
2018-12-31 16:06:48 -0700

    Added path validation for retrieving entries.

--- ppmtool/src/main/java/com/trizzo/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/com/trizzo/ppmtool/services/ProjectTaskService.java
@@ -19,49 +19,65 @@ public class ProjectTaskService {
 
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
-	
+
 	@Autowired
-	private ProjectRepository projectRepository; 
+	private ProjectRepository projectRepository;
 
 	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-		
+
 		try {
-		
-		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-		projectTask.setBacklog(backlog);
-		Integer BacklogSequence = backlog.getPTSequence();
-		BacklogSequence++;
-		backlog.setPTSequence(BacklogSequence);
 
-		projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + BacklogSequence);
-		projectTask.setProjectIdentifier(projectIdentifier);
+			Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+			projectTask.setBacklog(backlog);
+			Integer BacklogSequence = backlog.getPTSequence();
+			BacklogSequence++;
+			backlog.setPTSequence(BacklogSequence);
 
-		if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
-			projectTask.setStatus("TO_DO");
-		}
+			projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + BacklogSequence);
+			projectTask.setProjectIdentifier(projectIdentifier);
 
-		if (projectTask.getPriority() == null) {
-			projectTask.setPriority(3);
-		}
+			if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+				projectTask.setStatus("TO_DO");
+			}
 
-		return projectTaskRepository.save(projectTask);
-		} catch (Exception e){
+			if (projectTask.getPriority() == null) {
+				projectTask.setPriority(3);
+			}
+
+			return projectTaskRepository.save(projectTask);
+		} catch (Exception e) {
 			throw new ProjectNotFoundException("Project not found");
 		}
 	}
-	
-	public Iterable<ProjectTask>findBacklogById(String id) {
-		
+
+	public Iterable<ProjectTask> findBacklogById(String id) {
+
 		Project project = projectRepository.findByProjectIdentifier(id);
-		
-		if(project == null) {
+
+		if (project == null) {
 			throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
 		}
-		
+
 		return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
 	}
-	
+
 	public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
-		return projectTaskRepository.findByProjectSequence(pt_id);
+
+		Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+		if (backlog == null) {
+			throw new ProjectNotFoundException("Project with ID: '" + backlog_id + "' does not exist");
+		}
+
+		ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+		if (projectTask == null) {
+			throw new ProjectNotFoundException("Project Task '" + pt_id + "' not found.");
+		}
+
+		if (!projectTask.getProjectIdentifier().equals(backlog_id)) {
+			throw new ProjectNotFoundException(
+					"Project Task '" + pt_id + "' does not exist in project '" + backlog_id + "'.");
+		}
+
+		return projectTask;
 	}
 }
