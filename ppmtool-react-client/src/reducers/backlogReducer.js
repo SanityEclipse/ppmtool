fc70502a01317da2d1b267828c1bd7d9ce4ea3a0
trizzo <trizzo@payfone.com>
2019-01-04 14:56:15 -0700

    Added project task scaffolding and form for adding tasks.

---
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK
+} from "../actions/types";
+
+const initialState = {
+  projectTasks: [],
+  project_task: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_BACKLOG:
+      return {
+        ...state,
+        projectTasks: action.payload
+      };
+
+    case GET_PROJECT_TASK:
+      return {
+        ...state,
+        project_task: action.payload
+      };
+
+    case DELETE_PROJECT_TASK:
+      return {
+        ...state
+      };
+
+    default:
+      return state;
+  }
+}
