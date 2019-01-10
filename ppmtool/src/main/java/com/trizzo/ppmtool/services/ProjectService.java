e114e72e951791cc9705449ae37611fa0b766ace
trizzo <trizzo@payfone.com>
2019-01-10 13:16:02 -0700

    added logic for updating projects by user.

--- ppmtool/src/main/java/com/trizzo/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/trizzo/ppmtool/services/ProjectService.java
@@ -26,6 +26,17 @@ public class ProjectService {
 	
 	public Project saveOrUpdateProject(Project project, String username) {
 		
+		if(project.getId() != 0L) {
+			Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+			
+			if(existingProject != null && (!existingProject.getProjectLeader().equals(username))) {
+				throw new ProjectNotFoundException("Project not found in your account."); 
+			
+			} else if(existingProject == null) {
+				throw new ProjectNotFoundException("Project with ID: '" + project.getProjectIdentifier() + "' cannot be updated because it does not exist.");
+			}
+		}
+		
 		try {
 			
 			User user = userRepository.findByUsername(username);
