e8be6c892aa0a25486d10fdb4fdce07abc34bad8
trizzo <trizzo@payfone.com>
2019-03-28 12:57:42 -0600

    Changed project add and submit form.

--- ppmtool-react-client/src/components/project/AddProject.js
+++ ppmtool-react-client/src/components/project/AddProject.js
@@ -125,7 +125,7 @@ class AddProject extends Component {
                 </div>
                 <input
                   type="submit"
-                  className="btn btn-primary btn-block mt-4"
+                  className="btn btn-block mt-4 create-project-form"
                 />
               </form>
             </div>
